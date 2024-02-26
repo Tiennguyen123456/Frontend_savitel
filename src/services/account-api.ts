17cba2281cccd7ad69b4293b95fa1100a58342c0
Tan Le Minh <minhtan1581@gmail.com>
2024-02-26 16:43:49 +0700

    Rename storeAccount

--- src/services/account-api.ts
+++ src/services/account-api.ts
@@ -3,9 +3,9 @@ import { IAccountRes } from "@/models/api/account-api";
 import ApiRoutes from "./api.routes";
 
 const accountApi = {
-    storeAccout: async (body: any) => {
-        const response = await api.post<IResponse<IAccountRes>>(ApiRoutes.storeAccout, body);
-        return response.data;
+    storeAccount: async (body: any) => {
+        const response = await api.post<IResponse<IAccountRes>>(ApiRoutes.storeAccount, body);
+        return response;
     },
 };
 
